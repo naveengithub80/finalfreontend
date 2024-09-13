03e11f1dbb56d98480dae2c5cd2e28be2cd83ecb
Your Name <b.naveen9502@gmail.com>
2024-09-13 20:05:30 +0530

    initial commit

--- src/app/admindash/admindash.component.ts
+++ src/app/admindash/admindash.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee';
+
 
 @Component({
   selector: 'app-admindash',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './admindash.component.css'
 })
 export class AdmindashComponent {
+
+employees:Employee[]=[];
+  constructor(private employeeService:EmployeeService){}
+
+
+  ngOnInit():void{
+    this.getEmployees();
+  }
+
+  getEmployees(){
+    this.employeeService.getEmployeeList().subscribe(data=>{
+      this.employees=data;
+
+    })
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdmindashComponent } from './admindash/admindash.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { AdmindashComponent } from './admindash/admindash.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+    
   ],
   providers: [
     provideClientHydration()
